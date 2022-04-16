0c72d68250545eab26689fe2eedd8754d7afe17e
scarlette0305 <103047527+scarlette0305@users.noreply.github.com>
2022-04-16 15:05:02 +0700

    Add files via upload

--- pingpong_game.py
+++ pingpong_game.py
@@ -1,5 +1,5 @@
 from pygame import *
-from random import randint
+
 
 #region variables
 _WIDTH = 800
@@ -18,8 +18,12 @@ class GameSprite(sprite.Sprite):
     def draw(self, surface):
         surface.blit(self.image, (self.rect.x, self.rect.y))
 
+class Ball(GameSprite):
+    def update(self):
+        self.rect.x += self.speed
+
 class RectSprite(sprite.Sprite):
-    def __init__(self, filename, x,y, width, height,speed=0):
+    def __init__(self, color, x,y, width, height,speed=0):
         super().__init__()
         self.image = Surface((width, height))
         self.image.fill(color)
@@ -28,18 +32,48 @@ class RectSprite(sprite.Sprite):
     def draw(self, surface):
         surface.blit(self.image, (self.rect.x, self.rect.y))
 
-class Player(GameSprite):
+class Player(RectSprite):
     def update(self):
         keys = key.get_pressed()
-        if keys[K_LEFT]:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT]:
-            self.rect.x += self.speed
+        if keys[K_j]:
+            self.rect.y -= self.speed
+        if keys[K_k]:
+            self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.rect.y = 0
+        if self.rect.y > _HEIGHT - self.rect.height:
+            self.rect.y = _HEIGHT - self.rect.height
+
+
+class Player2(RectSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_f]:
+            self.rect.y -= self.speed
+        if keys[K_d]:
+            self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.rect.y = 0
+        if self.rect.y > _HEIGHT  - self.rect.height:
+            self.rect.y = _HEIGHT - self.rect.height
+
+ball = Ball("ball.png", 300,300,50,50,5)
+bar = Player('blue', _WIDTH-30, 300,30,150,speed=10)
+bar2 = Player2('blue', 0, 300,30,150,speed=10)
 
 game_is_running = True
-
-while game_is_running:
-    
+while game_is_running:    
     for e in event.get():
         if e.type == QUIT:
             game_is_running = False
+    window.fill((255,255,255))
+
+    bar.update()
+    bar.draw(window)
+    bar2.update()
+    bar2.draw(window)
+    ball.update()
+    ball.draw(window)
+
+    display.update()
+    clock.tick(60)
